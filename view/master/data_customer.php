7d76761f7df70fe36cd1147c26c5c806ff699bb2
Fajar Rahyudi Putra <fajarrahyu1103@gmail.com>
2024-07-02 21:16:05 +0700

    update data_customer

--- view/master/data_customer.php
+++ view/master/data_customer.php
@@ -39,98 +39,98 @@ include ('models/proses_customer.php');
                         while ($data = mysqli_fetch_assoc($hasil)) {
                             $id = $data['Id_Customer'];
                             ?>
-                            <tr>
-                                <td><?= $data['Nama_Customer'] ?></td>
-                                <td><?= $data['Telepon_Customer'] ?></td>
-                                <td><?= $data['Alamat_Customer'] ?></td>
-                                <td>
-                                    <a href="#" class="btn btn-info btn-sm" data-toggle="modal"
-                                        data-target="#edit<?= $id; ?>">
-                                        <i class="fas fa-pen"></i>
-                                    </a>
-                                    <a href="#" class="btn btn-danger btn-sm" data-toggle="modal"
-                                        data-target="#delete<?= $id; ?>">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
-                                </td>
-                            </tr>
+                        <tr>
+                            <td><?= $data['Nama_Customer'] ?></td>
+                            <td><?= $data['Telepon_Customer'] ?></td>
+                            <td><?= $data['Alamat_Customer'] ?></td>
+                            <td>
+                                <a href="#" class="btn btn-info btn-sm" data-toggle="modal"
+                                    data-target="#edit<?= $id; ?>">
+                                    <i class="fas fa-pen"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" data-toggle="modal"
+                                    data-target="#delete<?= $id; ?>">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
 
-                            <!-- MODAL EDIT CUSTOMER -->
-                            <div class="modal fade" id="edit<?= $id; ?>" tabindex="-1" role="dialog"
-                                aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Edit Data Customer</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <form method="POST">
-                                                <div class="form-group">
-                                                    <input type="hidden" class="form-control" id="ID_Customer"
-                                                        name="ID_Customer" value="<?= $id; ?>">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="Nama_Customer">Nama Customer</label>
-                                                    <input type="text" class="form-control" id="Nama_Customer"
-                                                        name="Nama_Customer" value="<?= $data['Nama_Customer'] ?>">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="Telepon_Customer">Telepon Customer</label>
-                                                    <input type="text" class="form-control" id="Telepon_Customer"
-                                                        name="Telepon_Customer" value="<?= $data['Telepon_Customer'] ?>">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="Alamat_Customer">Alamat Customer</label>
-                                                    <input type="text" class="form-control" id="Alamat_Customer"
-                                                        name="Alamat_Customer" value="<?= $data['Alamat_Customer'] ?>">
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-outline-primary"
-                                                        data-dismiss="modal">Close</button>
-                                                    <input type="submit" class="btn btn-primary" name="editCustomer"
-                                                        value="Simpan" />
-                                                </div>
-                                            </form>
-                                        </div>
+                        <!-- MODAL EDIT CUSTOMER -->
+                        <div class="modal fade" id="edit<?= $id; ?>" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Edit Data Customer</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form method="POST">
+                                            <div class="form-group">
+                                                <input type="hidden" class="form-control" id="ID_Customer"
+                                                    name="ID_Customer" value="<?= $id; ?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="Nama_Customer">Nama Customer</label>
+                                                <input type="text" class="form-control" id="Nama_Customer"
+                                                    name="Nama_Customer" value="<?= $data['Nama_Customer'] ?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="Telepon_Customer">Telepon Customer</label>
+                                                <input type="text" class="form-control" id="Telepon_Customer"
+                                                    name="Telepon_Customer" value="<?= $data['Telepon_Customer'] ?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="Alamat_Customer">Alamat Customer</label>
+                                                <input type="text" class="form-control" id="Alamat_Customer"
+                                                    name="Alamat_Customer" value="<?= $data['Alamat_Customer'] ?>">
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-outline-primary"
+                                                    data-dismiss="modal">Close</button>
+                                                <input type="submit" class="btn btn-primary" name="editCustomer"
+                                                    value="Simpan" />
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <!-- MODAL DELETE CUSTOMER -->
-                            <div class="modal fade" id="delete<?= $id; ?>" tabindex="-1" role="dialog"
-                                aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Hapus Data Customer</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <!--Form Hapus Customer-->
-                                            <form method="POST">
-                                                <div class="form-group">
-                                                    <p>Apakah anda yakin ingin menghapus
-                                                        <?= $data['Nama_Customer']; ?>?
-                                                        <input type="hidden" name="ID_Customer" class="form-control"
-                                                            value="<?= $id; ?>">
+                        <!-- MODAL DELETE CUSTOMER -->
+                        <div class="modal fade" id="delete<?= $id; ?>" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Hapus Data Customer</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <!--Form Hapus Customer-->
+                                        <form method="POST">
+                                            <div class="form-group">
+                                                <p>Apakah anda yakin ingin menghapus
+                                                    <?= $data['Nama_Customer']; ?>?
+                                                    <input type="hidden" name="ID_Customer" class="form-control"
+                                                        value="<?= $id; ?>">
 
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="submit" class="btn btn-outline-primary"
-                                                        data-dismiss="modal">Close</button>
-                                                    <input type="submit" class="btn btn-primary" name="hapusCustomer"
-                                                        value="Hapus">
-                                                </div>
-                                            </form>
-                                        </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="submit" class="btn btn-outline-primary"
+                                                    data-dismiss="modal">Close</button>
+                                                <input type="submit" class="btn btn-primary" name="hapusCustomer"
+                                                    value="Hapus">
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
+                        </div>
                         <?php } ?>
                     </tbody>
                 </table>
@@ -144,7 +144,7 @@ include ('models/proses_customer.php');
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Input Data Staff</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Input Data Customer</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
